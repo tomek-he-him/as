243fd6878c0ee3bbac97994448cc58f889114e89
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-23 13:39:01 +0100

    Array: working draft

---
+++ array.js
@@ -0,0 +1,18 @@
+//
+// map-to/array
+// -------------------------------------------------------------------------------------------------
+// Maps a `{a: b}` object to an array of `{key: a, value: b}` pairs.
+/**
+ * @param {Object} object – The object to be mapped
+ * @returns {Array} – A new array of key-value pairs mapped from the object
+ */
+module.exports = function mapToArray (object) {
+    var key;
+    var result = [];
+
+    for (key in object) if (object.hasOwnProperty(key)) {
+        result.push({key: key, value: object[key]});
+        }
+
+    return result;
+    };
