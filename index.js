3b03f42d46f36bd6f39270fb699aec062c76fd40
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-23 13:39:26 +0100

    Add an index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Consider importing individual functions:
+//     var mapToArray = require('map-to/array');
+
+module.exports =
+    { array: require('./array')
+    , object: require('./object')
+    };
