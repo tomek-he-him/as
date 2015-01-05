ee3b0fc08c97badac999e5411b9bf5960cceef71
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-01-05 20:11:09 +0100

    Add integration tests

--- test/array-object.js
+++ test/array-object.js
@@ -2,35 +2,69 @@ var test = require("tape");
 var asObject = require("../object");
 var asArray = require("../array");
 
-test("as/array » as/object", function (tape) {
+test("as/array >> as/object", function (tape) {
   var deepEqual = Function.prototype.apply.bind(tape.deepEqual, null);
 
 
-  // // Basic functionality
-  // // -----------------------------------------------------------------------------------------------
+  // Basic functionality
+  // -----------------------------------------------------------------------------------------------
 
-  // [ [ asObject(asArray({a: "b", c: "d"}))
-  //   , "should do the job for a simple array"
-  //   ]
+  [ [ asObject(asArray({a: "b", c: "d"}))
+    , {a: "b", c: "d"}
+    , "shouldn't change a simple object"
+    ]
 
-  // , [ asObject(asArray({}))
-  //   , "should return `[]` for an empty object"
-  //   ]
+  , [ asObject(asArray({}))
+    , {}
+    , "shouldn't change an empty object"
+    ]
 
-  // , [ asObject(asArray(
-  //     { a: null
-  //     , b: 0
-  //     , c: true
-  //     , d: false
-  //     , e: undefined
-  //     , f: "string"
-  //     , g: ""
-  //     , h: ["array"]
-  //     }))
-  //   , "should work for various data types"
-  //   ]
+  , [ asObject(asArray(
+      { a: null
+      , b: 0
+      , c: true
+      , d: false
+      , e: undefined
+      , f: "string"
+      , g: ""
+      , h: ["array"]
+      }))
+    , { a: null
+      , b: 0
+      , c: true
+      , d: false
+      , e: undefined
+      , f: "string"
+      , g: ""
+      , h: ["array"]
+      }
+    , "shouldn't change an object with various data types"
+    ]
 
-  // ].map(deepEqual);
+  , [ asObject(asArray
+      ( {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+      ))
+    , {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+    , "shouldn't change a nested object"
+    ]
+
+  , [ asObject(asArray
+      ( {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+      , {depth: 1}
+      ), {depth: 1})
+    , {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+    , "shouldn't change a nested object mapped to a specific depth"
+    ]
+
+  , [ asObject(asArray
+      ( {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+      , {depth: Infinity}
+      ), {depth: Infinity})
+    , {a: "b", c: "d", e: {f: "g", h: {i: "j"}}}
+    , "shouldn't change a nested object mapped deeply"
+    ]
+
+  ].map(deepEqual);
 
   tape.end();
   });
