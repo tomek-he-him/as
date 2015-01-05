8b281e519cd3bc95e97e6b984712382e0e1e88d2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-01-05 14:22:16 +0100

    Update jsdocs

--- array.js
+++ array.js
@@ -4,19 +4,22 @@
 // Maps an `{a: b}` object to a new array of `{key: a, value: b}` pairs.
 //
 /**
+ * @function asArray
+ *
  * @param {Object} object
- *    The object to be mapped
+ *    The object to be mapped.
  *
  * @param {Object} [options]
- *  - {Number} [depth=0]
- *      The depth to which the `object` tree should be mapped. Set it to `Infinity` to map the whole
- *      object's structure.
+ * - {Number} [depth=0]
+ *    The depth to which the `object` tree should be mapped. Set it to `Infinity` to map the whole
+ *    object's structure.
  *
- *  - {Boolean} [keepArrays=true]
- *      If set to false, arrays will be traversed like pure objects. `[true]` will be mapped to
- *      `[{key: "0", value: true}, {key: "length", value: 1}]`.
+ * - {Boolean} [keepArrays=true]
+ *    If set to false, arrays will be traversed like pure objects. `[true]` will be mapped to
+ *    `[{key: "0", value: true}, {key: "length", value: 1}]`.
  *
- * @returns {Array} – A new array of key-value pairs mapped from the object
+ * @returns {Array}
+ *    A new array of key-value pairs mapped from the object.
  */
 module.exports = function asArray (object) {
   var key;
--- object.js
+++ object.js
@@ -4,15 +4,18 @@
 // Maps an array of `{key: a, value: b}` pairs to a new `{a: b}` object.
 //
 /**
+ * @function asObject
+ *
  * @param {Array} array
  *    The array of key-value pairs to be mapped.
  *
  * @param {Object} [options]
- *  - {Number} [depth=0]
- *      The depth to which the `array`'s pairs should be traversed. Set it to `Infinity` to map the
- *      whole structure.
+ * - {Number} [depth=0]
+ *    The depth to which the `array`'s pairs should be traversed. Set it to `Infinity` to map the
+ *    whole structure.
  *
- * @returns {Array} – A new object mapped from the array.
+ * @returns {Object}
+ *    A new object mapped from the array.
  */
 module.exports = function asObject (array) {
   var i, l, pair;
