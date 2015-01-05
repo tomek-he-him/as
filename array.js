56bf373e958b3d1e7c408c2139e826a181c0b320
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-01-05 20:02:48 +0100

    Get rid of the option `traverseArrays`

--- array.js
+++ array.js
@@ -14,10 +14,6 @@
  *    The depth to which the `object`'s tree should be mapped. Set it to `Infinity` to map the
  *    entire tree structure.
  *
- * - {Boolean} [traverseArrays=false]
- *    If set to `true`, arrays will be traversed like pure objects. `[true]` will be mapped to
- *    `[{key: "0", value: true}]`.
- *
  * @returns {Array}
  *    A new array of key-value pairs mapped from the object.
  */
@@ -25,10 +21,9 @@ module.exports = function asArray (object, options, _depthLeft) {
   var key, value;
 
   if (!options) options = {};
-  var traverseArrays = options.traverseArrays || false;
   if (_depthLeft === void null && options.depth) _depthLeft = options.depth;
 
-  if (!traverseArrays && object instanceof Array) return object.slice();
+  if (object instanceof Array) return object.slice();
 
   var result = [];
   for (key in object) if (object.hasOwnProperty(key)) {
